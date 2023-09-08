d4e1be711c43a98503b1deeb4177de7782de4f64
saikrishnareddysanda <saikrishnasanda1@gmail.com>
2023-09-08 21:41:35 +0530

    Add hparam tuning and run experiments

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn import  metrics, svm
 from sklearn.model_selection import train_test_split
+from itertools import product
 
 def read_data():
     # Load the digits dataset
@@ -18,7 +19,7 @@ def split_train_dev_test(X, y, test_size, dev_size, random_state=1):
     # Split the data to train and (test + dev) sets
     X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=test_size + dev_size, random_state=random_state)
     # Split the (test + dev) data into test and dev sets
-    X_dev, X_test, y_dev, y_test = train_test_split(X_temp, y_temp, test_size=dev_size / (test_size + dev_size), random_state=random_state)
+    X_dev, X_test, y_dev, y_test = train_test_split(X_temp, y_temp, test_size=test_size / (test_size + dev_size), random_state=random_state)
     return X_train, X_dev, X_test, y_train, y_dev, y_test
 
 def train_model(X_train, y_train, model_parameters, model_type="svm"):
@@ -35,9 +36,31 @@ def predict_and_eval(model, X_test, y_test):
     report = metrics.classification_report(y_test, predicted)
     return predicted, report
 
+def get_acc(model, X, y):
+    predicted = model.predict(X)
+    return round(metrics.accuracy_score(y, predicted), 4)
+
 def visualize_confusion_matrix(y_test, predicted):
     disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
     disp.figure_.suptitle("Confusion Matrix")
     print(f"Confusion matrix:\n{disp.confusion_matrix}")
     # Display the confusion matrix
     plt.show()
+
+def create_combination_dictionaries_from_lists(dict_keys, value_lists):
+    result = [{name: value for name, value in zip(dict_keys, sublist)} for sublist in product(*value_lists)]
+    return result
+
+def tune_hparams(X_train, y_train, X_dev, y_dev, list_of_all_param_combination):
+    best_accuracy = -1
+
+    for model_params in list_of_all_param_combination:
+        model = train_model(X_train, y_train, model_params)
+        accuracy = get_acc(model, X_dev, y_dev)
+
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_model = model
+            best_hyperparameters = model_params
+
+    return best_hyperparameters, best_model, best_accuracy
