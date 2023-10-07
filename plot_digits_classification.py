56895e4134a40cadee0eaec2a69bff6785279cbe
saikrishnareddysanda <saikrishnasanda1@gmail.com>
2023-10-07 20:27:54 +0530

    Support decision tree

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -21,51 +21,71 @@ from utils import (
     create_combination_dictionaries_from_lists,
     tune_hparams,
 )
+import pandas as pd
 
 # Read images and labels from the dataset
 images, labels = read_data()
 
-gamma_ranges = [0.001, 0.01, 0.1, 1, 10, 100]
-C_ranges = [0.1, 1, 2, 5, 10]
-hyperparameter_names = ["gamma", "C"]
-hyperparameter_lists = [gamma_ranges, C_ranges]
+num_runs = 5
 
-list_of_all_param_combination = create_combination_dictionaries_from_lists(
-    hyperparameter_names, hyperparameter_lists
-)
+results_list = []
 
-test_size_list = [0.1, 0.2, 0.3]
-dev_size_list = [0.1, 0.2, 0.3]
+for run in range(num_runs):
+    run_dict_metrics = {}
+    for model_type in ['svm', 'tree']:
 
-print("Number of total samples in the datset:" , images.shape[0])
-print("Height of the image is:", images.shape[1])
-print("Width of the image is:", images.shape[2])
+        if model_type == 'svm':
+            gamma_ranges = [0.001, 0.01, 0.1, 1, 10, 100]
+            C_ranges = [0.1, 1, 2, 5, 10]
+            hyperparameter_names = ["gamma", "C"]
+            hyperparameter_lists = [gamma_ranges, C_ranges]
+        elif model_type == 'tree':
+            criterion = ['gini', 'entropy', 'log_loss']
+            max_depth = [None, 100, 200]
+            hyperparameter_names = ["criterion", "max_depth"]
+            hyperparameter_lists = [criterion, max_depth]   
 
-for test_size, dev_size in product(test_size_list, dev_size_list):
-    train_size = round((1 - (test_size + dev_size)), 2)
+        list_of_all_param_combination = create_combination_dictionaries_from_lists(
+            hyperparameter_names, hyperparameter_lists
+        )
 
-    # Split the data and labels into training, development, and testing sets
-    X_train, X_dev, X_test, y_train, y_dev, y_test = split_train_dev_test(
-        images, labels, test_size, dev_size
-    )
+        test_size_list = [0.2]
+        dev_size_list = [0.2]
 
-    # Preprocess the image data
-    X_train = preprocess_data(X_train)
-    X_dev = preprocess_data(X_dev)
-    X_test = preprocess_data(X_test)
+        # print("Number of total samples in the datset:" , images.shape[0])
+        # print("Height of the image is:", images.shape[1])
+        # print("Width of the image is:", images.shape[2])
 
-    best_hparams, best_model, best_dev_accuracy = tune_hparams(
-        X_train, y_train, X_dev, y_dev, list_of_all_param_combination
-    )
+        for test_size, dev_size in product(test_size_list, dev_size_list):
+            train_size = round((1 - (test_size + dev_size)), 2)
 
-    train_acc = get_acc(best_model, X_train, y_train)
-    test_acc = get_acc(best_model, X_test, y_test)
-    print(
-        "test_size = ", test_size,
-        "dev_size = ", dev_size,
-        "train_size = ", train_size,
-        "train_acc = ", train_acc,
-        "dev_acc = ", best_dev_accuracy,
-        "test_acc = ", test_acc
-    )
-    print("Best Hyperparamters:", best_hparams)
+            # Split the data and labels into training, development, and testing sets
+            X_train, X_dev, X_test, y_train, y_dev, y_test = split_train_dev_test(
+                images, labels, test_size, dev_size
+            )
+
+            # Preprocess the image data
+            X_train = preprocess_data(X_train)
+            X_dev = preprocess_data(X_dev)
+            X_test = preprocess_data(X_test)
+
+            best_hparams, best_model, best_dev_accuracy = tune_hparams(
+                X_train, y_train, X_dev, y_dev, list_of_all_param_combination, model_type
+            )
+
+            train_acc = get_acc(best_model, X_train, y_train)
+            test_acc = get_acc(best_model, X_test, y_test)
+            
+            metrics = {
+                "run":run,
+                "model_type": model_type,
+                "train_acc": train_acc,
+                "dev_acc": best_dev_accuracy,
+                "test_acc": test_acc
+            }
+            print(metrics)
+            results_list.append(metrics)
+
+print(results_list)
+print(pd.DataFrame(results_list))
+print(pd.DataFrame(results_list).groupby('model_type').describe().T)
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from sklearn import datasets
-from sklearn import  metrics, svm
+from sklearn import  metrics, svm, tree
 from sklearn.model_selection import train_test_split
 from itertools import product
 
@@ -17,14 +17,16 @@ def preprocess_data(images):
 
 def split_train_dev_test(X, y, test_size, dev_size, random_state=1):
     # Split the data to train and (test + dev) sets
-    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=test_size + dev_size, random_state=random_state)
+    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=test_size + dev_size, random_state=random_state, shuffle=True)
     # Split the (test + dev) data into test and dev sets
-    X_dev, X_test, y_dev, y_test = train_test_split(X_temp, y_temp, test_size=test_size / (test_size + dev_size), random_state=random_state)
+    X_dev, X_test, y_dev, y_test = train_test_split(X_temp, y_temp, test_size=test_size / (test_size + dev_size), random_state=random_state, shuffle=True)
     return X_train, X_dev, X_test, y_train, y_dev, y_test
 
 def train_model(X_train, y_train, model_parameters, model_type="svm"):
     if model_type == "svm":
         model = svm.SVC(**model_parameters)
+    elif model_type == "tree":
+        model = tree.DecisionTreeClassifier(**model_parameters)
     # Fit the model 
     model.fit(X_train, y_train)
     return model
@@ -51,11 +53,11 @@ def create_combination_dictionaries_from_lists(dict_keys, value_lists):
     result = [{name: value for name, value in zip(dict_keys, sublist)} for sublist in product(*value_lists)]
     return result
 
-def tune_hparams(X_train, y_train, X_dev, y_dev, list_of_all_param_combination):
+def tune_hparams(X_train, y_train, X_dev, y_dev, list_of_all_param_combination, model_type):
     best_accuracy = -1
 
     for model_params in list_of_all_param_combination:
-        model = train_model(X_train, y_train, model_params)
+        model = train_model(X_train, y_train, model_params, model_type)
         accuracy = get_acc(model, X_dev, y_dev)
 
         if accuracy > best_accuracy:
